5d4d6d863c43a045eea73e68af7d82cde88541b4
Prashant-Mutnale <prashantmutnale@gmail.com>
2019-04-11 00:03:20 +0530

    first commit redux

--- src/components/Layouts/header.js
+++ src/components/Layouts/header.js
@@ -27,7 +27,6 @@ class Header extends Component {
     if (!firebase.apps.length) {
       firebase.initializeApp(clientCredentials);
     }
-    this.props.new_posts();
 
     // if (this.state.user) this.addDbListener();
 
@@ -74,6 +73,7 @@ class Header extends Component {
   }
   clickme() {
     alert("caejd");
+    this.props.new_posts();
   }
   handleLogout() {
     firebase
