91ebad5ac191789e59c07492bd472695232b8090
Prashant-Mutnale <prashantmutnale@gmail.com>
2019-04-10 23:48:43 +0530

    first commit redux

--- src/redux/reducer/Postreducer.js
+++ src/redux/reducer/Postreducer.js
@@ -1,25 +1,25 @@
-import { NEW_POSTS, FETCH_POSTS, CREATE_POST } from '../actions/types'
+import { NEW_POSTS, FETCH_POSTS, CREATE_POST } from "../actions/types";
 
 const initialState = {
-	items: [],
-	item: {},
-	infosignup: {}
-}
+  items: [],
+  item: {},
+  infosignup: {}
+};
 
-export default function (state = initialState, action) {
-	switch (action.type) {
-		case NEW_POSTS:
-			console.log("got")
-			return {
-				...state,
-				items: action.payload
-			};
-		case CREATE_POST:
-			return {
-				...state,
-				item: action.payload
-			};
-		default:
-			return state;
-	}
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case NEW_POSTS:
+      console.log("got");
+      return {
+        ...state,
+        items: action.payload
+      };
+    case CREATE_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
+    default:
+      return state;
+  }
 }
